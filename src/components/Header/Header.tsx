50fa1661df969c39ed3fb65c2295091bc0d1e2e4
Zain Sherwani <180879518+ZainSherwaniDot@users.noreply.github.com>
2026-06-13 01:53:37 +0500

    Header updated

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,12 +10,25 @@ export default function Header() {
           <Image className={styles.logo} src="/logo120.png" alt="Logo" width={120} height={45} />
         </Link>
         <ul className={styles.navList}>
-          <li><Link href="/companies">Companies</Link></li>
+          {/* <li><Link href="/">Home</Link></li> */}
+          <li className={styles.dropdown}>
+            <Link className={styles.linkOfSubmenu} href="/companies">Companies</Link>
+            <ul className={styles.submenu}>
+              <li><Link href="/companies/nutech-automotive">Nutech Automotive</Link></li>
+              <li><Link href="/companies/nutech-distribution">Nutech Distribution</Link></li>
+              <li><Link href="/companies/nutech-telecom">Nutech Telecom</Link></li>
+              <li><Link href="/companies/nutech-solar">Nutech Solar & Power</Link></li>
+              <li><Link href="/companies/nutech-logistics">Nutech Logistics</Link></li>
+              <li><Link href="/companies/nutech-techonologies">Nutech Techonologies</Link></li>
+            </ul>
+          </li>
+          <li><Link href="/vendors">Vendors</Link></li>
           <li><Link href="/about">About</Link></li>
-          <li><Link href="/careers">Careers</Link></li>
+          <li><Link href="/blog">Blog</Link></li>
           <li><Link href="/contact">Contact</Link></li>
+          <li><Link href="/careers">Careers</Link></li>
         </ul>
-        <button className={styles.navCta}>Investor Relations</button>
+        <button className={styles.navCta}>Book an Appointment</button>
       </nav>
     </header>
   );
