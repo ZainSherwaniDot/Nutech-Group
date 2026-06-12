e8833386012e5c56127f4742b4201d1150342e42
Zain Sherwani <180879518+ZainSherwaniDot@users.noreply.github.com>
2026-06-13 02:32:05 +0500

    subtle changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -25,8 +25,8 @@ export default function Header() {
           <li><Link href="/vendors">Vendors</Link></li>
           <li><Link href="/about">About</Link></li>
           <li><Link href="/blog">Blog</Link></li>
-          <li><Link href="/contact">Contact</Link></li>
           <li><Link href="/careers">Careers</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
         </ul>
         <button className={styles.navCta}>Book an Appointment</button>
       </nav>
