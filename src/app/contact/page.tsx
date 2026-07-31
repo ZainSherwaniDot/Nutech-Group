93ae91068dd81480666e9a4a96cdc05784cea1e4
Zain Sherwani <180879518+ZainSherwaniDot@users.noreply.github.com>
2026-08-01 03:40:50 +0500

    added FinalCTA section to the contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,11 @@
 import MainSection from "@/components/contact/MainSection/MainSection";
+import FinalCTA from "@/components/home/FinalCTA/FinalCTA";
 
 export default function ContactPage() {
   return (
     <>
       <MainSection />
+      <FinalCTA />
     </>
   );
 }
