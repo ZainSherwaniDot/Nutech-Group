1cea235a75190ffa8ae2eec29d7c56700a9a4be4
Zain Sherwani <180879518+ZainSherwaniDot@users.noreply.github.com>
2026-08-17 22:52:56 +0500

    Mobile Navigation implemented

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,10 +3,12 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { Menu, SquareX } from 'lucide-react';
 import styles from "./Header.module.css";
 
 export default function Header() {
   const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -50,6 +52,30 @@ export default function Header() {
         </ul>
       </nav>
       <Link href="#final-cta-section" className={styles.navCta}>Book a Call</Link>
+
+      <button className={styles.menuButton} onClick={() => setMenuOpen((open) => !open)} type="button" aria-label="Toggle navigation" aria-expanded={menuOpen} > <Menu /> </button>
+
+      {menuOpen && (
+        <nav className={styles.mobileNav}>
+          <ul className={styles.mobileNavList}>
+            <li className={styles.mobileMenuClose}><Link href="" onClick={() => setMenuOpen(false)}><SquareX /></Link></li>
+            <li><Link href="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
+            <li><Link href="/companies" onClick={() => setMenuOpen(false)}>Companies</Link></li>
+            <li><Link href="/companies/nutech-automotive" onClick={() => setMenuOpen(false)}>- Nutech Automotive</Link></li>
+            <li><Link href="/companies/nutech-solar" onClick={() => setMenuOpen(false)}>- Nutech Solar & Power</Link></li>
+            <li><Link href="/companies/nutech-distribution" onClick={() => setMenuOpen(false)}>- Nutech Distribution</Link></li>
+            <li><Link href="/companies/nutech-logistics" onClick={() => setMenuOpen(false)}>- Nutech Logistics</Link></li>
+            <li><Link href="/companies/nutech-telecom" onClick={() => setMenuOpen(false)}>- Nutech Telecom</Link></li>
+            <li><Link href="/companies/nutech-technologies" onClick={() => setMenuOpen(false)}>- Nutech Technologies</Link></li>
+            <li><Link href="/vendors" onClick={() => setMenuOpen(false)}>Vendors</Link></li>
+            <li><Link href="/about" onClick={() => setMenuOpen(false)}>About</Link></li>
+            <li><Link href="/blog" onClick={() => setMenuOpen(false)}>Blog</Link></li>
+            <li><Link href="/careers" onClick={() => setMenuOpen(false)}>Careers</Link></li>
+            <li><Link href="/contact" onClick={() => setMenuOpen(false)}>Contact</Link></li>
+            <li><Link href="#final-cta-section" onClick={() => setMenuOpen(false)}>Book a Call</Link></li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 }
